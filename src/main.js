b3a63a9082bf1b43725664411a308d7938c8ba1b
Raulscj <92169825+Raulscj@users.noreply.github.com>
2022-03-10 17:58:05 -0400

    carrusel banner

--- src/main.js
+++ src/main.js
@@ -8,6 +8,36 @@ window.addEventListener("scroll", function () {
 });
 
 $(document).ready(function () {
+  /* Carrusel automático */
+  let i = 0;
+  setInterval(function () {
+    i += 1;
+    console.log("entro");
+    if (i == 1) {
+      $("#header").addClass("bg-1");
+      $("#header").removeClass("bg-2");
+      $("#header").removeClass("bg-3");
+      $("#header").removeClass("bg-4");
+    } else if (i == 2) {
+      $("#header").removeClass("bg-1");
+      $("#header").removeClass("bg-3");
+      $("#header").removeClass("bg-4");
+      $("#header").addClass("bg-2");
+    } else if (i == 3) {
+      $("#header").removeClass("bg-1");
+      $("#header").removeClass("bg-2");
+      $("#header").removeClass("bg-4");
+      $("#header").addClass("bg-3");
+    } else if (i == 4) {
+      $("#header").removeClass("bg-1");
+      $("#header").removeClass("bg-2");
+      $("#header").removeClass("bg-3");
+      $("#header").addClass("bg-4");
+    }
+    if (i > 4) {
+      i = 0;
+    }
+  }, 1500);
   /* Inicio */
   $("#boton_main").click(function () {
     var C = document.getElementById("Conocenos");
